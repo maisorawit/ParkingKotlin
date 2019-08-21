c002a5b5d2ee96a785e7317b4dd21b0658ef92de
maisorawit <maisorawit.st@gmail.com>
2019-08-21 18:28:44 +0700

    layout 2

--- app/src/main/java/buu/informatics/s59160135/parking/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160135/parking/MainActivity.kt
@@ -2,11 +2,26 @@ package buu.informatics.s59160135.parking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.confirm_button).setOnClickListener{
+            add(it)
+        }
+
+    }
+
+    private fun add(view: View){
+        val editText1 = findViewById<EditText>(R.id.block1_edit)
+        val editText2 = findViewById<EditText>(R.id.block2_edit)
+        val editText3 = findViewById<EditText>(R.id.block3_edit)
+
     }
 }
